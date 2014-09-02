3944b52103ac5dde4d93b457059996befdf9e7ec
vibhorrr <vibhormungee@Vibhors-MacBook-Air.local>
2014-09-02 09:52:56 +0530

    V2.0
    Parallex trial

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function equalHeight(group) {    
+    tallest = 0;    
+    group.each(function() {       
+        thisHeight = $(this).height();       
+        if(thisHeight > tallest) {          
+            tallest = thisHeight;       
+        }    
+    });    
+    group.each(function() { $(this).height(tallest); });
+} 
+
+$(document).ready(function() {   
+    equalHeight($(".thumbnail")); 
+});
