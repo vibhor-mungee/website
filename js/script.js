0ae2d3250c90353fa4cda0c57d9d10ca422355fd
vibhorrr <vibhormungee@Vibhors-MacBook-Air.local>
2014-09-07 13:00:40 +0530

    v3
    parallex implemented

--- js/script.js
+++ js/script.js
@@ -1,14 +1,42 @@
-function equalHeight(group) {    
-    tallest = 0;    
-    group.each(function() {       
-        thisHeight = $(this).height();       
-        if(thisHeight > tallest) {          
-            tallest = thisHeight;       
-        }    
-    });    
-    group.each(function() { $(this).height(tallest); });
-} 
+(function($){
 
-$(document).ready(function() {   
-    equalHeight($(".thumbnail")); 
-});
+	var $container = $(".parallax");
+	var $divs = $container.find("div.parallax-background");
+	var scrolledThing = document.body;
+	var liHeight = $divs.eq(0).closest("li").height();
+	var diffHeight= $divs.eq(0).height() - liHeight;
+	var len=$divs.length;
+	
+
+	var i,div,li,offset,scroll,top;
+
+
+	var offsets = $divs.get().map(function(div,d){
+		return $(div).offset();
+	})
+
+	var render = function(){
+
+		top = scrolledThing.scrollTop;
+		
+		for(i=0;i<len;i++){
+
+			div = $divs[i];
+
+			offset = top - offsets[i].top;
+
+			scroll = ~~(offset / liHeight * diffHeight);
+
+			transform = 'translate3d(0px, ' + (scroll*2) + 'px, 0px)';
+
+			div.style.webkitTransform = transform;
+		}
+	};
+
+	(function loop(){
+		requestAnimationFrame(loop);
+		render();
+	})();
+
+
+})(jQuery);
